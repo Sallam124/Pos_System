6cfc0411d9e8723d8a9b7dcd9041487479494c46
Sallam124 <Sallam.a.y.m@gmail.com>
2024-04-09 01:54:23 +0200

    Update data.py

--- Utilities.py/data.py
+++ Utilities.py/data.py
@@ -32,7 +32,16 @@ class DataTable(BoxLayout):
     def __init__(self,table='', **kwargs):
         super().__init__(**kwargs)
 
-        products = table
+        products = self.get_products()
+        
+
+         #     stb = {
+    #     'TH0':{0:'St0',1:'Sample1',2:'Sample2',3:'Sample4'},
+    #     'TH1':{0:'Stm0',1:'Sample1',2:'Sample2',3:'Sample4'},
+    #     'TH2':{0:'Stmp0',1:'Sampled1.0',2:'Sampled2.0',3:'Sampled4.0'},
+    #     'TH3':{0:'Stmpl0',1:'Sample1',2:'Sample2',3:'Sample4'},
+    #     'TH4':{0:'Stmple0',1:'Sample1',2:'Sample2',3:'Sample4'},
+
         col_titles = [k for k in products.keys()]
         rows_len = len(products[col_titles[0]])
         self.columns = len(col_titles)
@@ -47,4 +56,64 @@ class DataTable(BoxLayout):
         self.ids.table_floor_layout.cols = self.columns
         self.ids.table_floor.data = table_data
 
+    def get_products(self):
+        client = MongoClient()
+        db = client.Pos
+        products = db.stocks
+        _stocks = OrderedDict()
+        _stocks['product_code'] = {}
+        _stocks['product_name'] = {}
+        _stocks['product_weight'] = {}
+        _stocks['in_stock'] = {}
+        _stocks['sold'] = {}
+        _stocks['order'] = {}
+        _stocks['last_purchase'] = {}
 
+        product_code = []
+        product_name = []
+        product_weight = []
+        in_stock = []
+        sold = []   
+        order = []
+        last_purchase = []
+
+        for product in products.find():
+            product_code.append(product['product_code'])
+            name = product['product_name']
+            if len(name) > 10:
+                name = name[:10] + '...'
+            product_name.append(name)
+            product_weight.append(product['product_weight'])
+            in_stock.append(product['in_stock'])
+            sold.append(product['sold'])
+            order.append(product['order'])
+            last_purchase.append(product['last_purchase'])
+        # print(designations)
+        products_length = len(product_code)
+        idx = 0
+        while idx < products_length:
+            _stocks['product_code'][idx] = product_code[idx]
+            _stocks['product_name'][idx] = product_name[idx]
+            _stocks['product_weight'][idx] = product_weight[idx]
+            _stocks['in_stock'][idx] = in_stock[idx]
+            _stocks['sold'][idx] = sold[idx]
+            _stocks['order'][idx] = order[idx]
+            _stocks['last_purchase'][idx] = last_purchase[idx]
+           
+
+            idx += 1
+        
+        return _stocks
+        
+
+
+
+
+    
+class DataTableApp(App):
+    def build(self):
+
+        return DataTable()
+
+if __name__=='__main__':
+    DataTableApp().run()
