aebfb18116aa92c64a3084b468a71065ec7b4c2b
Sallam124 <Sallam.a.y.m@gmail.com>
2024-04-19 00:52:58 +0200

    Update Operation.py

--- Operator/Operation.py
+++ Operator/Operation.py
@@ -4,10 +4,15 @@ from kivy.uix.label import Label
 from kivy.clock import Clock
 from kivy.utils import get_color_from_hex
 import re
+from kivy.uix.modalview import ModalView
 from pymongo import MongoClient
 from kivy.lang import Builder
-
+from datetime import datetime
 # Builder.load_file('Operator/Operator.kv')
+class notify(ModalView):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.size_hint = (.7,.7)
 
 class Operation_Window(BoxLayout):
     def __init__(self,**kwargs):
@@ -15,132 +20,177 @@ class Operation_Window(BoxLayout):
         client = MongoClient()
         self.db = client.Pos    
         self.stocks = self.db.stocks
-
+        self.notify = notify()
+        self.Purchase_Records = self.db.Purchase_Records
         self.cart = []
         self.quantity = []
         self.total = 0.00
-        
+        Total = 0
+        self.post_tax = 0
     def logout(self):
         self.parent.parent.current = 'Screen_sign'
+    
+    def killswitch(self,dtx):
+        self.notify.dismiss()
+        self.notify.clear_widgets()
 
     def update_purchase(self):  
-        
-        pcode = self.ids.productcode.text # Get the product code entered by the user from kivy file
-        products_container = self.ids.products # Get the container where product details will be displayed from kivy file
+        pcode = self.ids.productcode.text
+        products_container = self.ids.products
+        last_record = self.db.Purchase_Records.find_one(sort=[('receipt_number', -1)])  
+    
+        receipt = last_record['receipt_number']
 
-        target_code = self.stocks.find_one({'product_code':pcode})
-        if target_code == None:
-            pass 
+        reciept_number = int(receipt[1:]) + 1
+
+
+        target_code = self.stocks.find_one({'product_code': pcode})
+        if target_code is None:
+            self.notify.add_widget(Label(text='[color=#FFFFFF][b]Product not found![/b][/color]', markup=True))
+            self.notify.open()
+            Clock.schedule_once(self.killswitch, 1) 
         else:
             # Create a new layout to hold the details of the product that is being added 
-            details = BoxLayout(size_hint_y= None,height=30,pos_hint={'top': 1})
+            details = BoxLayout(size_hint_y=None, height=30, pos_hint={'top': 1})
             products_container.add_widget(details)
-            product_price = target_code.get('product_price', 0)  # Accessing 'product_price' field from target_code, defaulting to 0 if not found
-            product_price = float(product_price)  # Convert product_price to float
-            Totall = product_price * 1.15
             
+            product_price = float(target_code.get('product_price', 0))
+            total_price = product_price * 1.15  # Assuming 15% tax, adjust if needed
 
-            code = Label(text=pcode ,size_hint_y= .7, size_hint_x = .1 , color = get_color_from_hex('#000000'))
-        
-            name = Label(text=target_code['product_name'] ,size_hint_y= .7, size_hint_x = .3, color = get_color_from_hex('#000000'))
-        
-            quantity = Label(text='1',size_hint_y= .7, size_hint_x = .1, color = get_color_from_hex('#000000'))
-        
-            discount = Label(text='0.00' ,size_hint_y= .7, size_hint_x = .1, color = get_color_from_hex('#000000'))
-        
-            price = Label(text=str(target_code['product_price']), size_hint_y=0.7, size_hint_x=0.1, color=get_color_from_hex('#000000'))
-
-            total = Label(text= str(Totall),size_hint_y= .7, size_hint_x = .2, color = get_color_from_hex('#000000') )
-        
+            code = Label(text=pcode, size_hint_y=.7, size_hint_x=.1, color=get_color_from_hex('#111212'))
+            name = Label(text=target_code['product_name'] , size_hint_y=.7, size_hint_x=.3, color=get_color_from_hex('#111212'))
+            quantity = Label(text="1", size_hint_y=.7, size_hint_x=.1, color=get_color_from_hex('#111212'))
+            discount = Label(text='0.0%', size_hint_y=.7, size_hint_x=.1, color=get_color_from_hex('#111212'))
+            price = Label(text=str(target_code['product_price']), size_hint_y=0.7, size_hint_x=0.1, color=get_color_from_hex('#111212'))
+            vat = Label(text='15%', size_hint=(0.1, 0.7), color=get_color_from_hex('#111212'))
+            total = Label(text="{:.3f}".format(total_price).rstrip('0').rstrip('.'), size_hint_y=.7, size_hint_x=.2, color=get_color_from_hex('#111212'))
+            
             details.add_widget(code)
-        
             details.add_widget(name)
-        
             details.add_widget(quantity)
-        
             details.add_widget(discount)
-        
             details.add_widget(price)
-        
+            details.add_widget(vat)
             details.add_widget(total)
 
-
-            pro_name = name.text
-
+            # Update cart
+            pro_name = name.text 
+            product_quantity = int(quantity.text)
             pro_price = float(price.text)
-            product_quantity_str = str('1')
-            self.total += pro_price * 1.15
-            purchase_Total = '\n\nTotal\t\t\t\t\t\t'+str(self.total)
+            Total = pro_price
+            self.total += pro_price 
+            self.total = round(self.total,2) 
+            self.post_tax += pro_price * 1.15
+            self.post_tax = round(self.post_tax,2)
+            purchase_Total = '\n\nSubTotal:\t\t\t\t\t\t\t\t\t\t\t\t\t\t ' + str(self.total) +'\nVat:\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t 15%\nTotal:\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t '+ str(self.post_tax) + '\n\t' 
             self.ids.product.text = pro_name
             self.ids.cur_price.text = str(pro_price)
 
             # Get the receipt preview widget and its current text
-            preview = self.ids.reciept_preview
-            previous = preview.text
-            prev = previous.find('Total')
-
-            # Update cart
-            if prev > 0:
-
-                previous = previous[:prev] # deletes any text found after prev , to print out updated reciept 
-
+            preview = self.ids.reciept_preview 
+            previous = preview.text  
+            prev = previous.find('SubTotal')
             
+            # Update reciept
+            if prev > 0:
+                previous = previous[:prev] # deletes any text found before prev , to print out updated reciept 
+                print(previous)
+        
             found_product = False
-            for i, code in enumerate(self.cart):        
+            for i, code in enumerate(self.cart):
                 if code == pcode:
                     found_product = True
 
+                    
             if found_product:
-                product_quantity_str = self.quantity[i] + 1
-                self.quantity[i] = product_quantity_str
+                self.quantity[i] += 1
+                product_quantity = self.quantity[i]
+                quantity.text = str(product_quantity)
+                Total = pro_price * product_quantity
+                Total = round(Total,2)
+                # Adjust the regular expression pattern to match the entire line, including "Total"
+                find_ = r'%s:\s+x\d+\s+\d+\.\d+\s+\d+\.\d+' % re.escape(pro_name)
 
-            # Construct expressions for finding and replacing the product quantity in the receipt text
-                find_ = '%s\t\tx\d\t' % (pro_name)
-                replace_ = pro_name + '\t\tx' + str(product_quantity_str) + '\t'
-            # Replace the existing product quantity in the receipt text with the updated quantity
+                # Construct the replacement string with placeholders for product name, quantity, product price, and total
+                replace_ = '%s: \t\t\t\tx%d\t\t\t\t\t%.2f\t\t\t %.2f' % (pro_name, product_quantity, pro_price, Total)
+
+                # Replace the existing product line in the receipt text with the updated line
                 new_text = re.sub(find_, replace_, previous)
-                preview.text = new_text + purchase_Total.replace('\n','')
+
+                preview.text = new_text + purchase_Total.replace('\n','',2)
+                #new line
             else:
                 self.cart.append(pcode)
                 self.quantity.append(1)
-            # Construct a new line for the product in the receipt text
-                new_text = ''.join([previous, pro_name + '\t\tx' + str(product_quantity_str) + '\t\t' + str(pro_price),'\nVat: 15% \n', purchase_Total])
-            # Update the receipt text with the new line
-                preview.text = new_text 
-            
+
+                # Construct a new line for the product in the receipt text
+                new_text = ''.join([previous , pro_name + ':  \t\t\t\tx' + str(product_quantity) + '\t\t\t\t\t' + str(pro_price),'\t\t\t ',str(Total), purchase_Total])
+                # Update the receipt text with the new line
+                preview.text = new_text
+
             self.ids.discount.text = '0.00'
-            self.ids.quantity.text = str(product_quantity_str)
+            self.ids.quantity.text = str(product_quantity)
             self.ids.price.text = str(pro_price)
             self.ids.vat.text = '15%'
-            self.ids.total.text = str(pro_price)
-
+            self.ids.total.text = str(total_price)
+            
     def update_database(self):
         # Deduct and update products in the database
         for item, quantity in zip(self.cart, self.quantity):
             target_code = self.stocks.find_one({'product_code': item})
             if target_code:
-                current_quantity = target_code.get('in_stock',0)
+                current_quantity = target_code.get('in_stock', 0)
                 new_quantity = current_quantity - quantity
                 
-                current_sold = target_code.get('sold',0)
-                print(current_sold)
+                current_sold = target_code.get('sold', 0)
                 current_sold += quantity
+
                 # Update the quantity in the database
-                print(current_sold)
-
                 self.stocks.update_one({'product_code': item}, {'$set': {'in_stock': new_quantity}})
-                self.stocks.update_many({'product_code':item},{'$set': {'sold': current_sold}})
-        # After deduction, clear the cart and quantity list
-        self.cart.clear()
-        self.quantity.clear()
-        # Reset the total
-        self.total = 0.00
+                self.stocks.update_many({'product_code': item}, {'$set': {'sold': current_sold}})
+        
+                # Insert a new record for the transaction
+                receipt_number = self.generate_receipt_number()  # Generate a new receipt number
+                total_amount = self.total
+                purchase_date = datetime.now().strftime("%Y-%m-%d")
+                items_purchased = [{'product_code': item, 'quantity': quantity} for item, quantity in zip(self.cart, self.quantity)]
 
+                # Create the purchase record
+                purchase_record = {
+                    'receipt_number': receipt_number,
+                    'total': total_amount,
+                    'date': purchase_date,
+                    'items_purchased': items_purchased
+                }
+
+                # Insert the record into the database
+                print(purchase_record)
+                self.db.Purchase_Records.insert_one(purchase_record)
+
+            # After deduction, clear the cart and quantity list
+            self.cart.clear()
+            self.quantity.clear()
+            # Reset the total
+            self.total = 0.00
+
+    def generate_receipt_number(self):
+        last_record = self.db.Purchase_Records.find_one(sort=[('receipt_number', -1)])
+        if last_record:
+            last_receipt_number = last_record['receipt_number']
+            # Extract the numeric part of the receipt number
+            last_number = int(last_receipt_number[1:])
+            # Increment the last number by 1
+            new_number = last_number + 1
+            # Construct the new receipt number
+            new_receipt_number = f"R{new_number:06d}" 
+            print(new_receipt_number)
+        return new_receipt_number
+    
     def reset_order(self):
         # Reset order details
         self.ids.product.text = 'Default Product'
         self.ids.cur_price.text = '0.00'
-        self.ids.reciept_preview.text = 'Super Serve \n 123 Banafseg.\n The 5th Settlement.Space\n\nTel:(20)10-2928-4678 \nReciept Number: 0001 \n Date: 4/16/2024 \n\n'
+        self.ids.reciept_preview.text = 'Super Serve \n 123 Banafseg.\n The 5th Settlement.Space\nTel:(20)10-2928-4678 \n Date: 4/16/2024 \n\n'
         self.ids.discount.text = '0.00'
         self.ids.quantity.text = '0'
         self.ids.price.text = '0.00'
