627bd25afdb079c2d094c4b3097113b8297f24ee
Sallam124 <Sallam.a.y.m@gmail.com>
2024-04-21 18:31:28 +0200

    Update Database.py

--- Database.py
+++ Database.py
@@ -14,15 +14,28 @@ for user in all_users:
     password = user['password']  # No need to decode
     print(f"Username: {username}, Password: {password}")
 
+first_name = input("Enter first name: ")
+last_name = input("Enter last name: ")
+user_name = input("Enter username: ")
+password = input("Enter password: ")
+designation = input("Enter designation: ")
+last_log = input("Enter last log date (YYYY-MM-DD): ")
 
+hashed_password = hashlib.sha256(password.encode()).hexdigest()
 
 new_user = {
-    "first_name": "New",
-    "last_name": "User",
-    "user_name": "Admin0",
-    "password": hashlib.sha256("Admin0".encode()).hexdigest(),  
-    "designation": "Administrator",  
-    "date": "2024-04-14"  
+    "first_name": first_name,
+    "last_name": last_name,
+    "user_name": user_name,
+    "password": hashed_password,
+    "designation": designation,
+    "last_log": last_log
 }
 users_collection.insert_one(new_user)
-print("New user added successfully!")
+
+if users_collection.inserted_id:
+    print("New user inserted successfully.")
+else:
+    print("Failed to insert new user.")
+
+
